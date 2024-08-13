387781df5f9c01ca3299d770d14abf75f352a330
Saideepsriram <saideepsriram2005@gmail.com>
2024-08-13 22:29:37 +0530

    yes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/<repository-name>/'  // Replace <repository-name> with your actual repository name
+  base: '/pass-generator/'  // Set the base path to your repository name
 })
